96e73a7f9d934588f68c434e7893944f409df09c
Mansour Behabadi <mansour@oxplot.com>
2018-11-28 22:59:26 +1100

    Add DIN 476 standard and sizes

--- sizes.go
+++ sizes.go
@@ -13,12 +13,14 @@ type Standard struct {
 var (
 	StandardISO216 = &Standard{"ISO-126"}
 	StandardISO269 = &Standard{"ISO-269"}
+	StandardDIN476 = &Standard{"DIN 476"}
 )
 
 // Standards maps standard names to instances of standards
 var Standards = map[string]*Standard{
 	"ISO-126": StandardISO216,
 	"ISO-269": StandardISO269,
+	"DIN 476": StandardDIN476,
 }
 
 // Size describes the attributes of a paper size
@@ -48,6 +50,9 @@ var (
 	SizeA8  = &Size{"A8", StandardISO216, 52, 74}
 	SizeA9  = &Size{"A9", StandardISO216, 37, 52}
 	SizeA10 = &Size{"A10", StandardISO216, 26, 37}
+	SizeA11 = &Size{"A11", StandardDIN476, 18, 26}
+	SizeA12 = &Size{"A12", StandardDIN476, 13, 18}
+	SizeA13 = &Size{"A13", StandardDIN476, 9, 13}
 
 	SizeB0  = &Size{"B0", StandardISO216, 1000, 1414}
 	SizeB1  = &Size{"B1", StandardISO216, 707, 1000}
@@ -60,6 +65,9 @@ var (
 	SizeB8  = &Size{"B8", StandardISO216, 62, 88}
 	SizeB9  = &Size{"B9", StandardISO216, 44, 62}
 	SizeB10 = &Size{"B10", StandardISO216, 31, 44}
+	SizeB11 = &Size{"B11", StandardDIN476, 22, 31}
+	SizeB12 = &Size{"B12", StandardDIN476, 15, 22}
+	SizeB13 = &Size{"B13", StandardDIN476, 11, 15}
 
 	SizeC0  = &Size{"C0", StandardISO269, 917, 1297}
 	SizeC1  = &Size{"C1", StandardISO269, 648, 917}
@@ -72,6 +80,19 @@ var (
 	SizeC8  = &Size{"C8", StandardISO269, 57, 81}
 	SizeC9  = &Size{"C9", StandardISO269, 40, 57}
 	SizeC10 = &Size{"C10", StandardISO269, 28, 40}
+
+	SizeD0 = &Size{"D0", StandardDIN476, 771, 1090}
+	SizeD1 = &Size{"D1", StandardDIN476, 545, 771}
+	SizeD2 = &Size{"D2", StandardDIN476, 385, 545}
+	SizeD3 = &Size{"D3", StandardDIN476, 272, 385}
+	SizeD4 = &Size{"D4", StandardDIN476, 192, 272}
+	SizeD5 = &Size{"D5", StandardDIN476, 136, 192}
+	SizeD6 = &Size{"D6", StandardDIN476, 96, 136}
+	SizeD7 = &Size{"D7", StandardDIN476, 68, 96}
+	SizeD8 = &Size{"D8", StandardDIN476, 48, 68}
+
+	Size4A0 = &Size{"4A0", StandardDIN476, 1682, 2378}
+	Size2A0 = &Size{"2A0", StandardDIN476, 1189, 1682}
 )
 
 // Sizes maps a paper size name to one or more Size instances
@@ -89,6 +110,9 @@ var Sizes = map[string][]*Size{
 	"A8":  {SizeA8},
 	"A9":  {SizeA9},
 	"A10": {SizeA10},
+	"A11": {SizeA11},
+	"A12": {SizeA12},
+	"A13": {SizeA13},
 
 	"B0":  {SizeB0},
 	"B1":  {SizeB1},
@@ -101,6 +125,9 @@ var Sizes = map[string][]*Size{
 	"B8":  {SizeB8},
 	"B9":  {SizeB9},
 	"B10": {SizeB10},
+	"B11": {SizeB11},
+	"B12": {SizeB12},
+	"B13": {SizeB13},
 
 	"C0":  {SizeC0},
 	"C1":  {SizeC1},
@@ -113,4 +140,17 @@ var Sizes = map[string][]*Size{
 	"C8":  {SizeC8},
 	"C9":  {SizeC9},
 	"C10": {SizeC10},
+
+	"D0": {SizeD0},
+	"D1": {SizeD1},
+	"D2": {SizeD2},
+	"D3": {SizeD3},
+	"D4": {SizeD4},
+	"D5": {SizeD5},
+	"D6": {SizeD6},
+	"D7": {SizeD7},
+	"D8": {SizeD8},
+
+	"4A0": {Size4A0},
+	"2A0": {Size2A0},
 }
